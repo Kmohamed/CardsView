810e9f8a0cdfb9e9ad0590ede70578bade85da4c
khaled El Morabeaa <khaledelmorabeaa@khaleds-MacBook-Pro-2.local>
2015-01-25 10:22:20 +0200

    Initial Commit

---
+++ CardsView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardsView
+//
+//  Created by khaled El Morabeaa on 1/25/15.
+//  Copyright (c) 2015 Brightunit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardsView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CardsView
+//
+//  Created by khaled El Morabeaa on 1/25/15.
+//  Copyright (c) 2015 Brightunit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
